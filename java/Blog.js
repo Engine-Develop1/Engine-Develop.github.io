5cc6add588240759e6d1e9f023f8a6a9e30f362c
bjorn espensen <bjorn@r66.dk>
2022-11-30 17:06:54 +0100

    makeing more stuff

--- java/Blog.js
+++ java/Blog.js
@@ -1,9 +1,6 @@
 function BlogP1()
 {
-    fetch('blog re/P1title.html')
-    .then(response=> response.text())
-    .then(text=> document.getElementById('P1title').innerHTML = text);
     fetch('blog re/P1BodyText.html')
     .then(response=> response.text())
-    .then(text=> document.getElementById('P1BodyText').innerHTML = text);
+    .then(text=> document.getElementById('text').innerHTML = text);
 }
